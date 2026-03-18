2419fa07529cdf7bf7a3df459543f7583b48aaf8
Bekam Guta <bekamdawit551@gmail.com>
2026-03-18 11:02:28 -0400

    packaging: include src package tree for installed CLI entry points

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     version="0.1.0",
     description="A TF-IDF + ML pipeline to classify movie plots by genre",
     author="Bekam Guta", # Or your actual name/organization
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(include=["src", "src.*"]),
     install_requires=[
         # you can either list your main deps here,
         # or leave this empty if you’ll rely on requirements.txt
